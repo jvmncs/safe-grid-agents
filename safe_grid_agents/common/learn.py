2f46749b72cdf8970ac216311b9b0cd696d38218
jason <jason@manc.us>
2018-09-05 01:55:19 -0400

    remove stray debug comments

--- safe_grid_agents/common/learn.py
+++ safe_grid_agents/common/learn.py
@@ -33,34 +33,13 @@ def tabq_learn(t, agent, env, env_state, history, args):
 
     # Act
     action = agent.act_explore(state)
-    # if t % 10000 == 0 or t % 10000 == 1 or t % 10000 == 2 or t % 10000 == 3 or t % 10000 == 4:
-    #     print('state', state['board'])
-    #     print('action', action)
-    #     state_board = tuple(state['board'].flatten())
-    #     print('value first', agent.Q[state_board])
-    #     print('value first', agent.Q[state_board])
-    #     print('value first', agent.Q[state_board])
     step_type, reward, discount, successor = env.step(action)
-
-    # if t % 10000 == 0 or t % 10000 == 1 or t % 10000 == 2 or t % 10000 == 3 or t % 10000 == 4:
-    #     print(t % 10000)
-    #     print('state', state['board'])
-    #     print('successor', successor['board'])
-    #     print(state['board'] == successor['board'])
-    #     print('action again', action)
-    #     print('reward', reward)
-    #     print('value', agent.Q[state_board])
-    #     print('action_explore result', agent.act_explore(state))
 
     # Learn
     if args.cheat:
         # TODO: fix this, since _get_hidden_reward ßeems to be episodic
         reward = env._get_hidden_reward()
-
     agent.learn(state, action, reward, successor)
-    # if t % 10000 == 0 or t % 10000 == 1 or t % 10000 == 2 or t % 10000 == 3 or t % 10000 == 4:
-    #     print('new value', agent.Q[state_board])
-    #     print()
 
     # Modify exploration
     eps = agent.update_epsilon()
