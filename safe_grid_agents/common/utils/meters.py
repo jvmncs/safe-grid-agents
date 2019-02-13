4883f3b96a86cb5b85551dda291ee2a8fb758e06
David Lindner <dev@davidlindner.me>
2019-02-13 15:09:16 +0100

    Debugging ppo-crmdp agent (#52)
    * better readability in if statement
    
    * fix bug in metric tracking
    
    - previously the environment was being reset after the last evaluation episode
    - this caused the call to track_metrics after the evaluation loop to track an
      additional episode with reward 0, which made the average be too low
    - this fixes it by not resetting the environment and only doing one
      call to track metrics for the last episode
    
    * bugfixing ppo-crmdp agent
    
    - define distance function for toy gridworlds based on manhattan distance
      between player positions
    - integrate corruption detection into gather_rollout from the ppo agent
      to be able to use the successor states, which we need
    - a bunch of minor fixes to the algorithm

--- safe_grid_agents/common/utils/meters.py
+++ safe_grid_agents/common/utils/meters.py
@@ -83,25 +83,26 @@ def track_metrics(history, env, eval=False, write=True):
             history["margins_support"].update(margin)
 
     # Write to Tensorboard
-    prefix = "Train/" if not eval else "Evaluation/"
-    writer = history["writer"]
-    if not eval and write:
-        writer.add_scalar("{}returns".format(prefix), history["returns"].val, ep)
-        if safety is not None:
-            writer.add_scalar("{}safeties".format(prefix), safety, ep)
-            writer.add_scalar("{}margins".format(prefix), margin, ep)
-            if margin > 0:
-                writer.add_scalar("{}margins_support".format(prefix), margin, ep)
-    elif eval and write:
-        # ep should be eval_period here (number of evals so far), not episode number
-        for kw in ["returns", "safeties", "margins", "margins_support"]:
-            if safety is None and kw != "returns":
-                continue
-            else:
-                writer.add_scalars(
-                    "{}{}".format(prefix, kw),
-                    {"avg": history[kw].avg, "max": history[kw].max},
-                    ep,
-                )
+    if write:
+        prefix = "Train/" if not eval else "Evaluation/"
+        writer = history["writer"]
+        if not eval:
+            writer.add_scalar("{}returns".format(prefix), history["returns"].val, ep)
+            if safety is not None:
+                writer.add_scalar("{}safeties".format(prefix), safety, ep)
+                writer.add_scalar("{}margins".format(prefix), margin, ep)
+                if margin > 0:
+                    writer.add_scalar("{}margins_support".format(prefix), margin, ep)
+        else:
+            # ep should be eval_period here (number of evals so far), not episode number
+            for kw in ["returns", "safeties", "margins", "margins_support"]:
+                if safety is None and kw != "returns":
+                    continue
+                else:
+                    writer.add_scalars(
+                        "{}{}".format(prefix, kw),
+                        {"avg": history[kw].avg, "max": history[kw].max},
+                        ep,
+                    )
 
     return history
